df5d6c7f5785219e7678be3153e33608ceb946bc
Tomasz Wrobel <tomaszm.wrobel@gmail.com>
2019-01-22 19:21:47 +0100

    rename file

--- src/main/java/com/tomwro/testapp/controller/HomeController.java
+++ src/main/java/com/tomwro/testapp/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 	
 	@GetMapping("/")
-	public String sayHello()
+	public String homePage()
 	{
 		return "home";
 	}
